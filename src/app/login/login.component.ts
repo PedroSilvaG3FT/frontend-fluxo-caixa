f87ddfc5f38742bfe88ab1c32d8b0a33a0a81ae4
Pedro Silva <pedro.silva-dev@hotmail.com>
2020-02-01 01:00:53 -0300

    iniciando home

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -7,13 +8,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
   isCollapsed = false;
-  constructor() { }
+  constructor(
+    private router: Router
+  ) { }
 
   ngOnInit() {
   }
 
   login() {
-    console.log("LOGIN")
+    console.log("LOGIN");
+    this.router.navigate(["home"]);
   }
 
 }
